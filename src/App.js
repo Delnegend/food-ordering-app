5c6be8bedb1657ec11a0b541e2b3389f153b1d58
Kwang1402 <hongquang2k4@gmail.com>
2023-05-24 09:38:49 +0700

    added Account Menu
    modified:   src/App.js
    new file:   src/HomePage.js
    new file:   src/ProfilePage.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,126 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import ProfilePage from './ProfilePage';
+import HomePage from './HomePage'
+import { Button } from '@mui/material';
 
-
-function HomePage() {
+function Home() {
+  const [showHome, setShowHome] = React.useState(true);
+  const handleHome = () => {
+    setShowHome(true);
+  }
   return (
-    <div>
-      <h1>UCC Food App</h1>
-    </div>
+    <React.Fragment>
+      <Button
+        onClick={handleHome}
+        style={{ position: 'absolute', top: 0, left: 20 }}
+      >
+        Home
+      </Button>
+      {showHome && <HomePage />}
+    </React.Fragment>
   );
 }
+function AccountMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-export default HomePage;
+  const [showProfile, setShowProfile] = React.useState(false);
+  const handleProfile = () => {
+    setShowProfile(true);
+  }
+  return (
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            style={{ position: 'absolute', top: 0, right: 0 }}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar> {/*userPicture*/}
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          style: {
+            right: 5
+          },
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 10,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleProfile}>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    {showProfile && <ProfilePage />}
+    </React.Fragment>
+  );
+}
+export {
+  AccountMenu, 
+  Home
+};
---
+++ src/ProfilePage.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function ProfilePage() {
+    return (
+        <div>Profile</div>
+    )
+}
+export default ProfilePage
