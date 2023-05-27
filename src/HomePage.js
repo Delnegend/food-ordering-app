489ef15a8700cf05ffb0235d7eae045a6ee3028f
Tonguesten36 <tung@tungsoft.net>
2023-05-27 14:07:38 +0700

    Added some Components to HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,36 @@
-import React from "react"
+import React from 'react';
 
-function HomePage() {
-    return (
-        <div
-        style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-            fontSize: '3rem',  
-            }}
-        >
-            Home page goes here
+import './Homepage.css';
+import { TextField, Accordion, AccordionSummary, Typography, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Homepage(){
+    return(
+        <div className="Homepage-wrapper">
+            <div className="Homepage-header">
+                <h1>UCC Food App</h1>
+            </div>
+            <div className="Homepage-body">
+                <TextField placeholder="Tìm kiếm" id="Homepage-body-search-bar"/>
+                <div className="Homepage-body-food-selection">
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                            >
+                            <Typography>Bánh mì</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Bánh mì thịt nướng
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            </div>
         </div>
     )
 }
-export default HomePage
+
+export default Homepage;
