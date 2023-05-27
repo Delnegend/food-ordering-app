339d86e2eba51a9e4d927dd83c52571d565bd903
Tonguesten36 <tung@tungsoft.net>
2023-05-27 15:16:42 +0700

    Put the Accordion component into a separate function

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 import './Homepage.css';
-import { TextField, Accordion, AccordionSummary, Typography, AccordionDetails } from '@mui/material';
+import { Container, TextField, Accordion, AccordionSummary, Typography, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function FoodAccordion(){
+    return(
+        <div className="Homepage-body-food-selection">
+            <Accordion id="BanhMi">
+                <AccordionSummary 
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header-banhmi"
+                >
+                    <Typography>Bánh mì</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Bánh mì đầy đủ</Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion id="Mi">
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header-mi"
+                >
+                    <Typography>Mì</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Mì tôm trứng & xúc xích</Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
 
 function Homepage(){
     return(
@@ -13,20 +44,9 @@ function Homepage(){
             <div className="Homepage-body">
                 <TextField placeholder="Tìm kiếm" id="Homepage-body-search-bar"/>
                 <div className="Homepage-body-food-selection">
-                    <Accordion>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                            >
-                            <Typography>Bánh mì</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                Bánh mì thịt nướng
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
+                    <Container>
+                        <FoodAccordion/>
+                    </Container>
                 </div>
             </div>
         </div>
