101001e815169597b66b3efac1b3e281be168f06
Kwang1402 <hongquang2k4@gmail.com>
2023-06-02 14:20:59 +0700

    Update Homepage.js
    Removed header

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -9,9 +9,9 @@ import FoodAccordion from './Components/FoodAccordion';
 function Homepage(){
     return(
         <div className="Homepage-wrapper">
-            <div className="Homepage-header">
+            {/* <div className="Homepage-header">
                 <h1>UCC Food App</h1>
-            </div>
+            </div> */}
             <div className="Homepage-body">
                 <TextField placeholder="Tìm kiếm" id="Homepage-body-search-bar"/>
                 <div className="Homepage-body-food-selection">
