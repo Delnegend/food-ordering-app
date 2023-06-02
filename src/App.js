dccb549b03b84da9fc2788c3276f9abb8fa89760
Kwang1402 <hongquang2k4@gmail.com>
2023-06-02 14:24:48 +0700

    Added Navbar
    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,129 +1,17 @@
 import * as React from 'react';
-import { useNavigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from './Homepage/Components/ProfilePage';
-import HomePage from './Homepage/Homepage';
-import { Button } from '@mui/material';
-
+import Homepage from './Homepage/Homepage';
+import Navbar from './Navbar/Navbar';
 function App() {
   return (
     <Router>
-      <Home />
-      <AccountMenu />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
     </Router>
-  );
-}
-function Home() {
-  let navigate = useNavigate();
-  return (
-    <React.Fragment>
-      <Tooltip title="Home">
-        <Button
-          onClick={() => navigate('/')}
-          style={{ position: 'absolute', top: 0, left: 20 }}
-        >
-          Home
-        </Button>
-      </Tooltip>
-    </React.Fragment>
-  );
-}
-function AccountMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  let navigate = useNavigate();
-  return (
-    <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            style={{ position: 'absolute', top: 0, right: 0 }}
-          >
-            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar> {/*userPicture*/}
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          style: {
-            right: 5
-          },
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 10,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem onClick={() => navigate('/profile')}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
-    </React.Fragment>
   );
 }
 
