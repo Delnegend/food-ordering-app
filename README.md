352e17738060da813f27f1928d23f9a1ff4a5b91
vietrux <leviettrung.social@gmail.com>
2023-06-09 12:18:48 +0700

    create new UI

--- README.md
+++
@@ -1,2 +0,0 @@
-# UCC-Food-app-UI
-Creating UI for UCC food app
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import FoodDetails from "./pages/FoodDetails";
+import Cart from "./pages/Cart";
+import MyOrders from "./pages/MyOrders";
+import Search from "./pages/Search";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/food" element={<FoodDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/myorders" element={<MyOrders />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
