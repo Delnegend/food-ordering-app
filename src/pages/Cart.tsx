f8fe3ba2baec67f4950c5e25090c6197183ca5f1
vietrux <leviettrung.social@gmail.com>
2023-06-15 01:04:42 +0700

    add FoodCard component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,11 +1,6 @@
-import Item from '../../components/cartCard.tsx'
-import '../../styles/cartCard.css'
-import banhmi from '../../public/banhmi.png'
-
 export default function Cart() {
     return (
         <>
-            <Item name="Bánh Mì" note="Trứng thịt xúc xích" price="20.000d" quantity="0" image={banhmi}/>
         </>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,80 @@
+import FoodCard from "../components/FoodCard/FoodCard"
+import CartCard from "../components/CartCard/CartCard"
 export default function Home() {
+    const foodsample = [
+        {
+            name: 'Bánh Mì',
+            info: {
+                delivery_price: 20000,
+                time: 30
+            },
+            taglist: ['Trứng', 'Thịt', 'Xúc xích'],
+            image: 'https://hips.hearstapps.com/hmg-prod/images/banh-mi-with-grilled-pork1-1663331872.jpg?crop=0.683xw:1.00xh;0.317xw,0&resize=1200:*'
+        },
+        {
+            name: 'Mì Ý',
+            info: {
+                delivery_price: 20000,
+                time: 30
+            },
+            taglist: ['Trứng', 'Thịt', 'Xúc xích'],
+            image: 'https://forza.com.vn/wp-content/uploads/2021/07/cach-lam-mi-y-thom-ngon-chuan-vi-tai-nha-6.jpeg'
+        }
+    ]
+    const cartsample = [
+        {
+            name: 'Bánh Mì',
+            note: 'Không ớt',
+            price: 20000,
+            image: 'https://hips.hearstapps.com/hmg-prod/images/banh-mi-with-grilled-pork1-1663331872.jpg?crop=0.683xw:1.00xh;0.317xw,0&resize=1200:*',
+            quantity: 1
+        },
+        {
+            name: 'Mì Ý',
+            note: 'Không ớt',
+            price: 20000,
+            image: 'https://forza.com.vn/wp-content/uploads/2021/07/cach-lam-mi-y-thom-ngon-chuan-vi-tai-nha-6.jpeg',
+            quantity: 1
+        }
+    ]
     return (
-        <></>
+        <>
+            <div style={
+                {
+                    display: 'flex',
+                    width: '100%',
+                    flexWrap: 'wrap',
+                }
+            }>
+                {
+                    foodsample.map((food, index) => {
+                        return (
+                            <FoodCard
+                                key={index}
+                                name={food.name}
+                                info={food.info}
+                                taglist={food.taglist}
+                                image={food.image}
+                            />
+                        )
+                    })
+                }
+                
+            </div>
+            {
+                    cartsample.map((food, index) => {
+                        return (
+                            <CartCard
+                                key={index}
+                                name={food.name}
+                                note={food.note}
+                                price={food.price}
+                                image={food.image}
+                                quantity={food.quantity}
+                            />
+                        )
+                    })
+                }
+        </>
     )
 }
