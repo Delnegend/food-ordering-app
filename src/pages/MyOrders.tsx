a1fbff434df787168cb4d294e1df0a5846f7a82d
Kwang1402 <hongquang2k4@gmail.com>
2023-06-23 04:22:34 +0700

    feat: add MyOrderCart

--- src/pages/MyOrders.tsx
+++ src/pages/MyOrders.tsx
@@ -1,3 +1,24 @@
+import MyOrderCard from "../components/MyOrderCard/MyOrderCard";
+import { ContainerCard } from "../assets/GlobalStyles";
+import { FoodList } from "../assets/GlobalTypes";
+import food_mock_data from "../_SAMPLE_DATA/food_mock.json";
+
 export default function MyOrders() {
-    return <></>;
+    const food_mock: FoodList = food_mock_data;
+    return (
+        <div style={ContainerCard}>
+            {Object.entries(food_mock).map(([key, value]) => {
+                return (
+                    <MyOrderCard
+                        key={key}
+                        name={value.name}
+                        price={value.price}
+                        prepare_time={value.prepare_time}
+                        taglist={value.taglist}
+                        image={value.image}
+                    />
+                );
+            })}
+        </div>
+    );
 }
