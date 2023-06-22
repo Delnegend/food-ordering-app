5b941fcfd2dc8f988ba1b8fbf278beea54d3dbf0
Delnegend <kiennguyen19323@gmail.com>
2023-06-23 01:26:20 +0700

    fix(CartCard, FoodCard): rewrite to match GlobalTypes

--- src/components/CartCard/CartCard.tsx
+++ src/components/CartCard/CartCard.tsx
@@ -1,31 +1,27 @@
 import styles from "./CartCard.module.css";
+import { FoodList } from "../../assets/GlobalTypes";
 
-type CartCardProps = {
-  name: string;
-  note: string;
-  price: number;
-  image: string;
+export default function CartCard(props: {
+  uuid: string;
   quantity: number;
-};
-function CartCard(props: CartCardProps) {
+  foodList: FoodList;
+}) {
   return (
-    <>
-      <div className={styles.container}>
-        <img className={styles.image} src={props.image} />
-        <div className={styles.body}>
-          <p className={styles.foodname}>{props.name}</p>
-          <p className={styles.note}>{props.note}</p>
-          <p className={styles.price}>{props.price}</p>
-          <div className={styles.quantity}>
-            <i className="fa-solid fa-circle-minus"></i>
-            <p className={styles.value}>{props.quantity}</p>
-            <i className="fa-solid fa-circle-plus"></i>
-          </div>
+    <div className={styles.container}>
+      <img
+        className={styles.image}
+        src={props.foodList[props.uuid].image}
+        alt={props.foodList[props.uuid].name}
+      />
+      <div className={styles.body}>
+        <p className={styles.foodname}>{props.foodList[props.uuid].name}</p>
+        <p className={styles.price}>{props.foodList[props.uuid].price}</p>
+        <div className={styles.quantity}>
+          <i className="fa-solid fa-circle-minus"></i>
+          <p className={styles.value}>{props.quantity}</p>
+          <i className="fa-solid fa-circle-plus"></i>
         </div>
       </div>
-    </>
+    </div>
   );
 }
-
-export default CartCard;
-export type { CartCardProps };
