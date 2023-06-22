eddd5b78cd90319d37209cc3f9d64942169b1fe9
Delnegend <kiennguyen19323@gmail.com>
2023-06-23 01:26:20 +0700

    fix(CartCard, FoodCard): rewrite to match GlobalTypes

--- src/components/CartCard/Total.tsx
+++ src/components/CartCard/Total.tsx
@@ -4,15 +4,11 @@ type TotalProps = {
   total: number;
 };
 
-function Total(props: TotalProps) {
+export default function Total(props: TotalProps) {
   return (
-    <>
-      <div className={styles.container}>
-        <p className={styles.totalText}>Tổng Tiền:</p>
-        <p className={styles.price}>{props.total}</p>
-      </div>
-    </>
+    <div className={styles.container}>
+      <p className={styles.totalText}>Tổng tiền:</p>
+      <p className={styles.price}>{props.total}</p>
+    </div>
   );
 }
-
-export default Total;
--- src/components/FoodCard/FoodCard.tsx
+++ src/components/FoodCard/FoodCard.tsx
@@ -1,38 +1,28 @@
 import styles from "./FoodCard.module.css";
-type FoodCardProps = {
-  name: string;
-  info: {
-    delivery_price: number;
-    time: number;
-  };
-  taglist: string[];
-  image: string;
-};
-function FoodCard(props: FoodCardProps) {
+import { FoodItem } from "../../assets/GlobalTypes";
+
+export default function FoodCard(props: FoodItem) {
   return (
-    <>
-      <div className={styles.container}>
-        <img className={styles.image} src={props.image} />
-        <div className={styles.body}>
-          <p className={styles.foodname}>{props.name}</p>
-          <div className={styles.info}>
-            <i className="fa-solid fa-moped"></i>
-            <span>{props.info.delivery_price} </span>
-            <i className="fa-solid fa-clock"></i>
-            <span>{props.info.time} mins</span>
-          </div>
-          <div className={styles.taglist}>
-            {props.taglist.map((tag, index) => {
-              return (
-                <p className={styles.tag} key={index}>
-                  {tag}
-                </p>
-              );
-            })}
-          </div>
+    <div className={styles.container}>
+      <img className={styles.image} src={props.image} />
+      <div className={styles.body}>
+        <p className={styles.foodname}>{props.name}</p>
+        <div className={styles.info}>
+          <i className="fa-solid fa-moped"></i>
+          <span>{props.price} ₫</span>
+          <i className="fa-solid fa-clock"></i>
+          <span>{props.prepare_time} phút</span>
+        </div>
+        <div className={styles.taglist}>
+          {props.taglist.map((tag, index) => {
+            return (
+              <p className={styles.tag} key={index}>
+                {tag}
+              </p>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
 }
-export default FoodCard;
