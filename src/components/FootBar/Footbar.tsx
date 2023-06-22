b6f9409f1140d14a4c85c2c3087989f95611360c
Delnegend <kiennguyen19323@gmail.com>
2023-06-22 23:33:44 +0700

    refactor(Footbar): remove unnecessary, repetitive codes

--- src/components/FootBar/Footbar.tsx
+++ src/components/FootBar/Footbar.tsx
@@ -3,70 +3,36 @@ import { useNavigate } from "react-router-dom";
 import styles from "./FootBar.module.css";
 export default function FootBar() {
   const navigate = useNavigate();
-  const [activeIcon, setActiveIcon] = useState<number>(1);
-  const handleIconClick = (index: number) => {
-    setActiveIcon(index);
-    navigate(getPageName(index));
+
+  const buttonData: { icon: string; page: string }[] = [
+    { icon: "fa-compass", page: "/" },
+    { icon: "fa-location-dot", page: "/" },
+    { icon: "fa-shopping-bag", page: "/" },
+    { icon: "fa-heart", page: "/" },
+    { icon: "fa-bell", page: "/myorders" },
+  ];
+  const [currentPageIdx, setActiveIdx]: [number, (index: number) => void] =
+    useState<number>(1);
+
+  const handleIconClick = (pageIdx: number) => {
+    setActiveIdx(pageIdx);
+    navigate(buttonData[pageIdx].page);
   };
-  const getPageName = (index: number) => {
-    switch (index) {
-      case 0:
-        return "/";
-      case 1:
-        return "/";
-      case 2:
-        return "/";
-      case 3:
-        return "/myorders";
-      default:
-        return "";
-    }
-  };
+
   return (
     <nav className={styles.footbar}>
-      <ul className={`${styles["icon-list"]}`}>
-        <li>
-          <i
-            className="fa-solid fa-compass fa-2xl"
-            style={{ color: "#FF7F50", cursor: "default" }}
-          />
-        </li>
-        <li>
-          <button onClick={() => handleIconClick(0)}>
-            <i
-              className={`${styles["fa-solid"]} ${
-                activeIcon === 0 ? styles["active"] : ""
-              } fa-solid fa-location-dot fa-2xl`}
-            />
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleIconClick(1)}>
-            <i
-              className={`${styles["fa-solid"]} ${
-                activeIcon === 1 ? styles["active"] : ""
-              } fa-solid fa-shopping-bag fa-2xl`}
-            />
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleIconClick(2)}>
-            <i
-              className={`${styles["fa-solid"]} ${
-                activeIcon === 2 ? styles["active"] : ""
-              } fa-solid fa-heart fa-2xl`}
-            />
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleIconClick(3)}>
-            <i
-              className={`${styles["fa-solid"]} ${
-                activeIcon === 3 ? styles["active"] : ""
-              } fa-solid fa-bell fa-2xl`}
-            />
-          </button>
-        </li>
+      <ul className={styles["icon-list"]}>
+        {buttonData.map((button, pageIdx) => (
+          <li key={pageIdx}>
+            <button onClick={() => handleIconClick(pageIdx)}>
+              <i
+                className={`${styles["fa-solid"]} ${
+                  currentPageIdx === pageIdx ? styles["active"] : ""
+                } fa-solid fa-2xl ${button.icon}`}
+              />
+            </button>
+          </li>
+        ))}
       </ul>
     </nav>
   );
