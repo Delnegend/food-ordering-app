02d859e5221fa30cc4a5633cd4cda7dd4d36d140
Delnegend <kiennguyen19323@gmail.com>
2023-06-23 01:20:40 +0700

    feat(assets): adding GlobalStyles and GlobalTypes

---
+++ src/assets/GlobalStyles.tsx
@@ -0,0 +1,8 @@
+const ContainerCard: React.CSSProperties = {
+  display: "flex",
+  width: "100%",
+  flexWrap: "wrap",
+  marginBottom: "60px",
+};
+
+export { ContainerCard };
---
+++ src/assets/GlobalTypes.tsx
@@ -0,0 +1,23 @@
+type FoodItem = {
+  name: string;
+  price: number;
+  prepare_time: number;
+  taglist: string[];
+  image: string;
+};
+
+type FoodList = {
+  [key: string]: FoodItem;
+};
+
+type CartList = {
+  [key: string]: number;
+};
+
+type RouteList = {
+  icon: string;
+  path: string;
+  page: JSX.Element;
+}[];
+
+export type { FoodItem, FoodList, CartList, RouteList };
