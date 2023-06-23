a06cc110d59eb6dc7acf4da2ddc607820387fda1
Delnegend <kiennguyen19323@gmail.com>
2023-06-23 14:56:07 +0700

    refactor(Cart, Home, MyOrders): rm GlobalStyles, seperate scss modules for flexibility

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,9 @@
 import CartCard from "../components/CartCard/CartCard";
 import Total from "../components/CartCard/Total";
 import { CartList, FoodList } from "../assets/GlobalTypes";
-import { ContainerCard } from "../assets/GlobalStyles";
-
 import cart_mock_data from "../_SAMPLE_DATA/cart_mock.json";
 import food_mock_data from "../_SAMPLE_DATA/food_mock.json";
+import styles from "./Cart.module.scss";
 
 const TotalSum = (food: FoodList, cart: CartList) => {
     let total = 0;
@@ -19,7 +18,7 @@ export default function Cart() {
     const food_mock: FoodList = food_mock_data;
 
     return (
-        <div style={ContainerCard}>
+        <div className={styles.container}>
             {Object.entries(cart_mock).map(([uuid, quantity]) => {
                 return (
                     <CartCard
