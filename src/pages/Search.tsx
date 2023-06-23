167c4c3ec9dfb1076ea2949ca658125da5a65635
Delnegend <kiennguyen19323@gmail.com>
2023-06-23 17:15:15 +0700

    feat(Search): adding searchbox

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,8 @@
+import styles from "./Search.module.scss";
 export default function Search() {
-    return <></>;
+    return (
+        <div className={styles.container}>
+            <input type="text" placeholder="🔍 Tìm kiếm món ăn" />
+        </div>
+    );
 }
