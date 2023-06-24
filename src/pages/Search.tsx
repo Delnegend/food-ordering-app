2240b0ffd4f639cc91478203bdfb596733cbdc01
Delnegend <kiennguyen19323@gmail.com>
2023-06-24 18:58:31 +0700

    feat(Search): removing search page

--- src/pages/Search.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Search.module.scss";
-export default function Search() {
-    return (
-        <div className={styles.container}>
-            <input type="text" placeholder="🔍 Tìm kiếm món ăn" />
-        </div>
-    );
-}
