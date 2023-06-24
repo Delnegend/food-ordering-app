fcd237111e8341f683becadc2cc4565f7bbacf44
vietrux <leviettrung.social@gmail.com>
2023-06-25 02:30:54 +0700

    signin page UI

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,15 @@
+import styles from "./SignIn.module.scss";
 export default function SignIn() {
-    return <></>;
+    return (
+        <>
+            <div className={styles.container}>
+
+                <button className={styles.btn}>
+                    <i className="fab fa-google"></i>
+                    Sign In with Google
+                </button>
+            </div>
+
+        </>
+    );
 }
