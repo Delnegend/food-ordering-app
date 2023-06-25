00004ce420deb6203b3832fcfe273af9b742d3ad
Delnegend <kiennguyen19323@gmail.com>
2023-06-25 10:36:41 +0700

    refactor(Home, App, NavBar): re-style, mv NavBar HomeTopBar, mv HTP from App to Home

---
+++ src/components/HomeTopBar/HomeTopBar.tsx
@@ -0,0 +1,108 @@
+import { useState, useRef, useEffect } from "react";
+import styles from "./HomeTopBar.module.scss";
+
+type HomeTopBarProps = {
+    appName: string;
+    avatarUrl: string;
+};
+export type { HomeTopBarProps };
+
+export default function HomeTopBar({ appName, avatarUrl }: HomeTopBarProps) {
+    const [isHamburgerMenuActive, setActiveHamburger] = useState(false);
+    const navRef = useRef<HTMLDivElement>(null);
+
+    const toggleHamburgerMenu = () => {
+        setActiveHamburger(!isHamburgerMenuActive);
+    };
+
+    const handleOutsideClick = (event: MouseEvent) => {
+        if (navRef.current && !navRef.current.contains(event.target as Node)) {
+            setActiveHamburger(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick);
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, []);
+
+    const subMenus = {
+        "My Orders": "fa-rectangle-list",
+        "My Profile": "fa-user",
+        "Delivery Address": "fa-location-dot",
+        "Payment Methods": "fa-wallet",
+        "Contact Us": "fa-envelope",
+        "Sign Out": "fa-sign-out",
+    };
+
+    let avatarElement = <i className="fa-solid fa-user fa-2xl"></i>;
+    if (avatarUrl) {
+        avatarElement = <img src={avatarUrl} />;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.hamburger_menu_container}>
+                <button
+                    className={styles.toggle_btn}
+                    onClick={toggleHamburgerMenu}
+                >
+                    <i className="fa-solid fa-bars fa-2xl"></i>
+                </button>
+
+                <nav
+                    className={`${styles.hamburger_menu} ${
+                        isHamburgerMenuActive ? styles.active : ""
+                    }`}
+                    ref={navRef}
+                >
+                    <ul className={`${styles["tab-list"]}`}>
+                        <li>
+                            {avatarElement}
+                        </li>
+                        <li>
+                            <span className={styles.username}>User Name</span>
+                        </li>
+                        <li>
+                            <span className={styles.email}>
+                                username@gmail.com
+                            </span>
+                        </li>
+                        {Object.entries(subMenus).map(([key, value]) => {
+                            return (
+                                <li key={key}>
+                                    <button>
+                                        <i
+                                            className={`fa-duotone ${value} fa-2xl`}
+                                        ></i>
+                                        <span>{key}</span>
+                                    </button>
+                                </li>
+                            );
+                        })}
+                        <li>
+                            <button className={`${styles["log-out-button"]}`}>
+                                <i
+                                    className={`${styles["log-out-icon"]} fa-regular fa-power-off fa-xl`}
+                                ></i>
+                                <span className={`${styles["log-out"]}`}>
+                                    {" "}
+                                    Log Out{" "}
+                                </span>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className={styles.app_name_container}>
+                <span>{appName}</span>
+            </div>
+
+            <div className={styles.avatar_container}>{avatarElement}</div>
+        </div>
+    );
+};
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import FoodCard from "../components/FoodCard/FoodCard";
-import { FoodList } from "../assets/GlobalTypes";
 import Label from "../components/CategoryLabel/Label";
+import HomeTopBar from "../components/HomeTopBar/HomeTopBar";
+import { FoodList } from "../assets/GlobalTypes";
+
 import food_mock_data from "../_SAMPLE_DATA/food_mock.json";
+import styles from "./Home.module.scss";
+
 type FoodType = {
     name: string;
     icon: string;
 };
-import styles from "./Home.module.scss";
 
 export default function Home() {
     const food_mock: FoodList = food_mock_data;
@@ -15,9 +18,13 @@ export default function Home() {
         { name: "Xôi", icon: "fa-bowl-rice" },
         { name: "Mì trộn", icon: "fa-bowl-chopsticks-noodles" },
     ];
+    const welcome_message = "Bạn muốn ăn gì?";
+    const appName = "UCC Food App";
 
     return (
         <div className={styles.container}>
+            <HomeTopBar avatarUrl="" appName={appName} />
+            <div className={styles.welcome_message}>{welcome_message}</div>
             <div className={styles.label_container}>
                 {food_types.map((value, index) => {
                     return (
