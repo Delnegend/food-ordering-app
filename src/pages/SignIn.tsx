bb353a1dde21f5655191c7a9cc0c38cb0fc63e25
Delnegend <kiennguyen19323@gmail.com>
2023-06-25 10:42:11 +0700

    style: lint

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,13 +3,11 @@ export default function SignIn() {
     return (
         <>
             <div className={styles.container}>
-
                 <button className={styles.btn}>
                     <i className="fab fa-google"></i>
                     Sign In with Google
                 </button>
             </div>
-
         </>
     );
 }
