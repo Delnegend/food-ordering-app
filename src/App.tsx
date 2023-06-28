25d09b3fa90994988258c21a8acc92dc180c1c4e
Delnegend <kiennguyen19323@gmail.com>
2023-06-28 18:51:25 +0700

    feat(Cart, App): adding logic to Cart page, move states in Cart to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import Home from "./pages/Home";
 import { HomePageProps } from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Cart from "./pages/Cart";
+import { CartProps, CartList } from "./pages/Cart";
 import MyOrders from "./pages/MyOrders";
 import FootBar from "./components/FootBar/Footbar";
 import { FootbarProps } from "./components/FootBar/Footbar";
 import FoodDetails from "./pages/FoodDetails";
 import { FoodDetailsProps } from "./pages/FoodDetails";
 import { foodDetailPath } from "./assets/GlobalVariables";
+import { useState, useEffect } from "react";
 
 import food_mock_data from "./_SAMPLE_DATA/food_mock.json";
 
 export default function App() {
     const HomePageData: HomePageProps = {
-        food_list: food_mock_data,
-        food_types: [
+        foodList: food_mock_data,
+        foodTypes: [
             { name: "Bánh mì", faIcon: "fa-baguette", type: "bread" },
             { name: "Xôi", faIcon: "fa-bowl-rice", type: "rice" },
             {
@@ -31,14 +33,32 @@ export default function App() {
         userEmail: "",
     };
 
+    const [cartItems, setCartItems] = useState<CartList>({});
+
+    const CartPageData: CartProps = {
+        foodList: food_mock_data,
+        cartItems: cartItems,
+        setCartItems: setCartItems,
+    };
+
+    useEffect(() => {
+        setCartItems({
+            "6fdd237b-6b0e-429e-9722-411ae1099753": 1,
+            "fef8b604-577e-4adc-98ce-c5fe98debbb6": 2,
+        });
+    }, []);
+
     const routes: FootbarProps = [
         { icon: "fa-house", path: "/", page: <Home {...HomePageData} /> },
-        { icon: "fa-cart-shopping", path: "/cart", page: <Cart /> },
+        {
+            icon: "fa-cart-shopping",
+            path: "/cart",
+            page: <Cart {...CartPageData} />,
+        },
         { icon: "fa-list", path: "/myorders", page: <MyOrders /> },
         { icon: "fa-user", path: "/signin", page: <SignIn /> },
     ];
 
-    // const cartItems: { [uuid: string]: number } = {}
     const foodDetailsProps: FoodDetailsProps = {
         cart: {},
         foodList: food_mock_data,
