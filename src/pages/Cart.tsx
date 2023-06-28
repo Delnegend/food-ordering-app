42e218fea261f4f5613f8ab674cc24aada9dbfaf
Delnegend <kiennguyen19323@gmail.com>
2023-06-28 23:00:08 +0700

    feat(Cart page): re-style, add logic

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,6 +3,8 @@ import food_mock_data from "../_SAMPLE_DATA/food_mock.json";
 import styles from "./Cart.module.scss";
 import { FoodList } from "../components/FoodCard/FoodCard";
 import { vibrateDuration } from "../assets/GlobalVariables";
+import { readablePrice } from "../assets/GlobalVariables";
+import { useEffect, useState } from "react";
 
 type CartList = {
     [uuid: string]: number;
@@ -52,32 +54,58 @@ export default function Cart(props: CartProps) {
         );
     };
 
+    const [isCartEmpty, setIsCartEmpty] = useState<boolean>(true);
+    useEffect(() => {
+        setIsCartEmpty(Object.keys(props.cartItems).length === 0);
+    }, [props.cartItems]);
+    const emptyCartClass = isCartEmpty ? styles["cart-empty"] : "";
+
     return (
         <div className={styles.container}>
-            <div className={styles["cart-items"]}>
-                {Object.entries(props.cartItems).map(([uuid, quantity]) => {
-                    return (
-                        <CartCard
-                            key={uuid}
-                            uuid={uuid}
-                            foodList={foodList}
-                            quantity={quantity}
-                            setQuantity={(quantity: number) => {
-                                setQuantity(uuid, quantity);
-                            }}
-                        />
-                    );
-                })}
+            <div
+                className={`${styles["cart-container__top"]} ${emptyCartClass}`}
+            >
+                <div className={styles["cart-items"]}>
+                    {Object.entries(props.cartItems).map(([uuid, quantity]) => {
+                        return (
+                            <CartCard
+                                key={uuid}
+                                uuid={uuid}
+                                foodList={foodList}
+                                quantity={quantity}
+                                setQuantity={(quantity: number) => {
+                                    setQuantity(uuid, quantity);
+                                }}
+                            />
+                        );
+                    })}
+                </div>
+                <div className={styles["cart-total"]}>
+                    <span className={styles["cart-total-text"]}>
+                        Tổng cộng:
+                    </span>
+                    <span className={styles["cart-total-price"]}>
+                        {readablePrice(totalSum(foodList, props.cartItems))}
+                    </span>
+                </div>
             </div>
-            <div className={styles["cart-total"]}>
-                <span className={styles["cart-total-text"]}>Tổng cộng:</span>
-                <span className={styles["cart-total-price"]}>
-                    {totalSum(foodList, props.cartItems)}
+            <div
+                className={`${styles["cart-container__bottom"]} ${emptyCartClass}`}
+            >
+                <button
+                    className={styles["cart-checkout-btn"]}
+                    onClick={checkOut}
+                >
+                    Thanh toán
+                </button>
+            </div>
+            <div
+                className={`${styles["cart-container__empty"]} ${emptyCartClass}`}
+            >
+                <span className={styles["cart-empty-text"]}>
+                    Chưa có món nào trong giỏ hàng
                 </span>
             </div>
-            <button className={styles["cart-checkout-btn"]} onClick={checkOut}>
-                Thanh toán
-            </button>
         </div>
     );
 }
