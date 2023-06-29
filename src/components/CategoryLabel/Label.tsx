b1a7a433913b9f5946dc60b1a1a6f7c5f9a10c51
Delnegend <kiennguyen19323@gmail.com>
2023-06-29 15:38:37 +0700

    feat(Label.tsx): add logic, rename className to match prev commit

--- src/components/CategoryLabel/Label.tsx
+++ src/components/CategoryLabel/Label.tsx
@@ -3,17 +3,33 @@ type LabelProps = {
     name: string;
     type: string;
     faIcon: string;
+    currentActiveLabel: string;
+    setCurrentActiveLabel: (label: string) => void;
 };
 
 export type { LabelProps };
 
 export default function Label(props: LabelProps) {
+    const activeClass =
+        props.currentActiveLabel === props.type
+            ? styles["label__container--active"]
+            : "";
+
+    const handleActiveLabel = () => {
+        if (props.currentActiveLabel === props.type)
+            props.setCurrentActiveLabel("");
+        else props.setCurrentActiveLabel(props.type);
+    };
+
     return (
-        <div className={styles.label}>
-            <div className={styles.icon}>
+        <button
+            className={`${styles["label__container"]} ${activeClass}`}
+            onClick={handleActiveLabel}
+        >
+            <div className={styles["label-icon__container"]}>
                 <i className={`fa-solid ${props.faIcon}`}></i>
             </div>
-            <div className={styles.text}>{props.name}</div>
-        </div>
+            <div className={styles["label-text"]}>{props.name}</div>
+        </button>
     );
 }
