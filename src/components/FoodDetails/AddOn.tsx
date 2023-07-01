8e0f2b9e88c95a7fee7566ce5b7ebb196aa04ea2
BuildTools <unconfigured@null.spigotmc.org>
2023-07-01 15:37:36 +0700

    Add Food Details Addon Ingredients

---
+++ src/components/FoodDetails/AddOn.tsx
@@ -0,0 +1,21 @@
+import { FoodItem } from "../FoodCard/FoodCard";
+import styles from "./AddOn.module.scss"
+import { readablePrice } from "../../assets/GlobalVariables";
+
+
+
+export default function AddOn(props: FoodItem) {
+    return (        
+        <div className={styles.container}>
+            <div className={styles["details-image-container"]}>
+                <img className={styles["details-image"]} src={props.image} alt={props.name} />
+            </div>
+            <div className={styles["details-title"]}>{props.name}</div>
+            <div className={styles["details-price-quantity"]}>
+                <div className={styles["details-price"]}>
+                    +{readablePrice(props.price)}đ
+                </div>
+            </div>
+        </div>
+        )
+}
